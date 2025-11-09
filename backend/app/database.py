217a1bbe380b4d1809ef11e526994750a3ea7600
mehinp <mehinpandya@gmail.com>
2025-11-08 19:18:46 -0500

    public db

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+aiomysql://root:Database123!@localhost:3306/nfl_stocks"
+DATABASE_URL = os.getenv("MYSQL_PUBLIC_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
